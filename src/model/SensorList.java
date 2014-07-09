586ed80898d7cccdb5a4b4e5a3cf63aaaeb913a9
rp17 <rp31@txstate.edu>
2014-07-09 15:27:03 -0500

    2 rp17 cm1

--- src/model/SensorList.java
+++ src/model/SensorList.java
@@ -1,5 +1,8 @@
 package model;
 
 public class SensorList {
-	public void print(){System.out.println("Sensor list");}
+	public void print(){
+		System.out.println("Sensor list");
+		System.out.println("2_RP17 Sensor 1");
+		}
 }
